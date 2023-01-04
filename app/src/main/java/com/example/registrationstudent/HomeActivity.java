bfa3a99f969abd4cd1e30216739e195843588f00
Anushka <anushkashinde417@gmail.com>
2023-01-05 00:48:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/registrationstudent/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.registrationstudent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by tutlane on 08-01-2018.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
